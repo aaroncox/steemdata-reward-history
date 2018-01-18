1e85c7a8c94cea6b6278035ed0f18e0c3039591d
Aaron Cox <aaron.cox@greymass.com>
2018-01-18 12:24:00 -0500

    added connection string to command, to avoid needing local mongodb running

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Starting steem history export, this will take a while!"
 echo "... exporting author rewards"
-mongo --quiet queries/author_rewards.js > output/author_rewards.csv
+mongo mongodb://mongo1.steemdata.com:27017/SteemData -u steemit -p steemit --quiet queries/author_rewards.js > output/author_rewards.csv
 echo "... exporting benefactor rewards"
-mongo --quiet queries/beneficiary_rewards.js > output/beneficiary_rewards.csv
+mongo mongodb://mongo1.steemdata.com:27017/SteemData -u steemit -p steemit --quiet queries/beneficiary_rewards.js > output/beneficiary_rewards.csv
 echo "... exporting curation rewards"
-mongo --quiet queries/curation_rewards.js > output/curation_rewards.csv
+mongo mongodb://mongo1.steemdata.com:27017/SteemData -u steemit -p steemit --quiet queries/curation_rewards.js > output/curation_rewards.csv
 echo "... exporting witness rewards"
-mongo --quiet queries/witness_rewards.js > output/witness_rewards.csv
+mongo mongodb://mongo1.steemdata.com:27017/SteemData -u steemit -p steemit --quiet queries/witness_rewards.js > output/witness_rewards.csv
 echo "Completed, CSVs can be found within the ./output folder. Running the script again will replace these files, so if you're performing this for multiple accounts make sure to copy them first."
